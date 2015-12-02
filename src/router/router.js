e879498f8cca0c8c8e6eea2b6c2ed9ec706292d1
王政 <wangzheng4@staff.weibo.com>
2015-12-02 09:16:55 +0800

    调整方法命名；添加route.back功能

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,26 @@
 //router资源
 
+//import core/notice
+//import core/fixUrl
 //import ./base
 //import ./listen
 
-var router_api = {
+var router_router_api = {
     set: router_listen_setRouter,
-    get: router_get
+    get: router_router_get,
+    back: router_router_back
 };
 
-function router_get() {
+function router_router_get() {
     return router_base_params;
+}
+
+function router_router_back(url) {
+  history.back();
+  if (url) {
+    core_notice_on('routerChange', function routerChange() {
+      core_notice_off('routerChange', routerChange);
+      history.replaceState(router_listen_lastStateData, null, core_fixUrl(router_router_get().url, url));
+    });
+  }
 }
